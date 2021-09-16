62738e7eac875984f8b2afc27d3abe72c1977140
Crowdbotics <team@crowdbotics.com>
2021-09-16 13:20:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "samuel_test_cancel__30601Identifier",
+  url: "https://samuel_test_cancel__30601Identifier.botics.co",
+  api: "https://samuel_test_cancel__30601Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
